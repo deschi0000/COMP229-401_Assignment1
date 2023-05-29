96d0fda68e8232fbd6f2e39a907891458584644d
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-05-29 09:26:09 +0800

    add text animation

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -9,3 +9,20 @@
     window.addEventListener("load", Start);
 
 })();
+
+
+$(() => {
+    console.log("from the query!");
+
+    // fade in the titles
+    $("h1").animate({
+        opacity: 1
+    }, 1500)
+    $("h3").animate({
+        opacity: 1
+    }, 2750)
+
+
+
+
+})
