08fb9e1911744ab9064add18e82c085ea840d771
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-05-29 13:47:52 +0800

    animate thankyou and form

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,4 +1,5 @@
 // IIFE -- Immediately Invoked Function Expression
+// Scripts file for Frontend
 // Dave von Deschwanden - 301303220 - 05.2023
 
 (function(){
@@ -14,9 +15,10 @@
 
 
 $(() => {
-    console.log("from the query!");
 
-    // fade in the titles
+    // Main Page
+
+    // Fade in the titles
     $("h1").animate({
         opacity: 1
     }, 1500)
@@ -24,7 +26,14 @@ $(() => {
         opacity: 1
     }, 2750)
 
+    // Contact Page
+    $("form").animate({
+        opacity: 1
+    }, 1000)
 
-
+    //Thank you
+    $(".thankyou").show(1000)
+    .slideUp(1500);
+    
 
 })
